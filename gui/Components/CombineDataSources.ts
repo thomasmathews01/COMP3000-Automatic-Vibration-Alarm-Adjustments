fbe5065e58a0d4686797b8f379be13571c4d85c6
Thomas Mathews <thomas.mathews@beraninstruments.com>
2020-11-23 21:28:25 +0000

    Initial TypeScript-React-NextJS login page

--- gui/Components/CombineDataSources.ts
+++ gui/Components/CombineDataSources.ts
@@ -1,9 +1,5 @@
 import {alarmLevelChange, dataItem} from '../Types/DataTypes'
 
-/*
-    We have an expectation that the request time of the alarm changes will contain an entry at time = 0 with the level at that moment in time.
- */
-
 export const addStateData = (data: dataItem[]) => {
     if (!data || data.length < 2)
         return data;
@@ -18,14 +14,25 @@ export const addStateData = (data: dataItem[]) => {
 };
 
 export const addAlarmData = async (data: dataItem[], alarms: alarmLevelChange[], alerts: alarmLevelChange[]) => {
-    const alertLevels = await getLevels(data, alerts);
-    const alarmLevels = await getLevels(data, alarms);
+    const levels = await Promise.all([getLevels(data, alerts), getLevels(data, alarms)]);
 
     return data.map((point, index) => {
-        return {...point, ...{alarmLevel: alarmLevels[index], alertLevel: alertLevels[index]}}
+        return {...point, ...{alarmLevel: levels[1][index], alertLevel: levels[0][index]}}
     });
 }
 
-export const getLevels = async (data: dataItem[], alarms: alarmLevelChange[], levels?: number[]): Promise<number[]> => {
-    if (alarms.length == 0) return levels ?? [];
+export const getLevels = async (data: dataItem[], alarms: alarmLevelChange[]): Promise<number[]> => {
+    alarms = alarms.filter(y => data.some(x => x.secondsSinceEpoch >= y.secondsSinceEpoch)); // Remove nonsense values
+
+    if (alarms.length == 0) return [];
+
+    const pivotIndices = alarms.map(alarm => data.findIndex(dataPoint => dataPoint.secondsSinceEpoch >= alarm.secondsSinceEpoch));
+
+    const getAllPointsBeforeNextPivot = (pivot: number, indexOfPivot: number) => {
+        const nextPivot = pivotIndices[indexOfPivot + 1] ?? data.length; // If there is no next pivot, take all values until the end.
+        const countOfPointsDuringAlarmPeriod = nextPivot - pivot;
+        return new Array(countOfPointsDuringAlarmPeriod).fill(alarms[indexOfPivot].level);
+    }
+
+    return pivotIndices.map(getAllPointsBeforeNextPivot).reduce((x, y) => x.concat(y), []);
 }
