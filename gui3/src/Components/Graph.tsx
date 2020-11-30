db2ffa921e2c771a288e1a225bcaf1748235c595
Thomas Mathews <thomas.mathews@beraninstruments.com>
2020-11-30 11:20:22 +0000

    Implements zoom ability

--- gui3/src/Components/Graph.tsx
+++ gui3/src/Components/Graph.tsx
@@ -1,8 +1,16 @@
 import React, {useState} from 'react';
-import {HorizontalGridLines, LineSeries, LineSeriesPoint, VerticalGridLines, XAxis, XYPlot, YAxis} from 'react-vis'
+import {Highlight, HighlightArea, HorizontalGridLines, LineSeries, LineSeriesPoint, VerticalGridLines, XAxis, XYPlot, YAxis} from 'react-vis'
 import '../../node_modules/react-vis/dist/style.css';
 import {dataItem} from "../Types/DataTypes"; // TODO: Abstract network types into their own file to make API changes easier.
 import axios from "axios";
+import {Button} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        background: theme.palette.secondary.main,
+    }
+}));
 
 const generateAreaData = () => {
     const sampleCount = 5000;
@@ -27,6 +35,7 @@ const generateAreaData2 = () => {
     ];
 }
 
+
 const fetchValueData = async (channel: number, type: number) => {
     const response = await axios.get<{ data: dataItem[] }>(`http://localhost:3456/data?channel=${channel}&type=${type}`);
     if (response.data.data)
@@ -44,30 +53,78 @@ interface PropsT {
 
 const Graph = (props: PropsT) => {
     const [valuesData, setValuesData] = useState<LineSeriesPoint[]>([]);
+    const [currentArea, setCurrentArea] = useState<HighlightArea>({top: 100, bottom: -100, left: 0, right: new Date().getTime()}); // TODO: Set initials sensibly so we can cope with the initial lack of values
+    const classes = useStyles();
+
+
+    function updateDragArea(area: HighlightArea | null) {
+        if (area !== undefined && area !== null && currentArea !== undefined && setCurrentArea !== undefined) {
+            if (area.left !== undefined && area.right !== undefined && area.bottom !== undefined && area.top !== undefined) {
+                const fullRange = getFullRange(valuesData);
+                area.bottom = Math.max(area.bottom, fullRange.bottom);
+                area.top = Math.max(area.top, fullRange.top);
+                area.left = Math.max(area.left, fullRange.left);
+                area.right = Math.max(area.right, fullRange.right);
+            }
+
+            setCurrentArea(area ?? {});
+        }
+    }
+
+    const getFullRange = (points: LineSeriesPoint[]) => {
+        return {
+            left: points[0].x, right: points[points.length - 1].x,
+            bottom: Math.min.apply(null, points.map(p => p.y)),
+            top: Math.max.apply(null, points.map(p => p.y))
+        };
+    }
+
+    const displayFullRange = (points: LineSeriesPoint[]) => {
+        setCurrentArea(getFullRange(points));
+    }
 
     if (valuesData.length === 0)
         fetchValueData(props.channel, props.type)
-            .then((result) => setValuesData(result))
-            .then(() => console.log("Fetch complete."));
+            .then((result) => {
+                setValuesData(result);
+                displayFullRange(result);
+            })
+            .then(() => console.log("Fetch complete."))
 
     console.log("Rendering...");
     return (
-        <XYPlot xType={"time"}
-                width={850}
-                height={300}>
-            <HorizontalGridLines/>
-            <VerticalGridLines/>
-            <LineSeries data={valuesData}/>
-            <XAxis tickLabelAngle={-90}/>
-            <YAxis/>
-        </XYPlot>
+        <div>
+            <XYPlot xType={"time"}
+                    width={850}
+                    height={300}
+                    xDomain={
+                        currentArea && [
+                            currentArea.left,
+                            currentArea.right
+                        ]
+                    }
+                    yDomain={
+                        currentArea && [
+                            currentArea.bottom,
+                            currentArea.top
+                        ]
+                    }>
+                <HorizontalGridLines/>
+                <VerticalGridLines/>
+                <LineSeries data={valuesData}/>
+                <XAxis tickLabelAngle={-90}/>
+                <YAxis/>
+                <Highlight drag={true} onDragEnd={updateDragArea}/>
+            </XYPlot>
+            <br/>
+            <Button onClick={() => displayFullRange(valuesData)} className={classes.button}>Reset Zoom</Button>
+        </div>
     );
 }
 
 export default Graph;
 
 /*
-
             <AreaSeries data={generateAreaData()}/>
             <AreaSeries data={generateAreaData2()}/>
  */
