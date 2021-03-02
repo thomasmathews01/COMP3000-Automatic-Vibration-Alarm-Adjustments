fb82e8394e8b4cde346ab1c51471ebc3f496970d
Thomas Mathews <thomas.mathews@beraninstruments.com>
2021-03-02 16:31:24 +0000

    Implement current alarm state at the site level

--- Middleware/RESTInterface/NetworkRequestHandlers/CurrentAlarmStateRequest.cpp
+++ Middleware/RESTInterface/NetworkRequestHandlers/CurrentAlarmStateRequest.cpp
@@ -27,18 +27,31 @@ std::string get_alarm_severity_as_json(const alarmSeverity severity) {
 
 crow::response CurrentAlarmStateRequest::get_current_alarm_state(const crow::request& request, const std::shared_ptr<IConfigurationAccess>& config_storage,
                                                               const std::shared_ptr<IAlarmStorage>& alarm_storage) {
-	auto machine_id = CrowExtractionHelpers::extract_int_from_url_params(request, "machine_id");
-	const auto channel_id = CrowExtractionHelpers::extract_int_from_url_params(request, "channel_id");
-	machine_id = machine_id ? machine_id : config_storage->get_machine_id_from_channel_id(*channel_id);
+	std::vector<int> machine_ids;
+	
+	if (const auto machine_id = CrowExtractionHelpers::extract_int_from_url_params(request, "machine_id"); machine_id)
+		machine_ids.push_back(*machine_id);
 
-	if (!machine_id)
+	const auto channel_id = CrowExtractionHelpers::extract_int_from_url_params(request, "channel_id");
+	if (channel_id)
+		machine_ids.push_back(config_storage->get_machine_id_from_channel_id(*channel_id));
+
+	if (const auto site_id = CrowExtractionHelpers::extract_int_from_url_params(request, "site_id"); site_id) {
+		const auto machines = config_storage->get_machines_for_site(site(*site_id));
+		std::transform(machines.cbegin(), machines.cend(), std::back_inserter(machine_ids), [](const machine& machine) { return machine.id; });
+	} // TODO: Unit test cases for the various possibilities of inputs
+
+	if (machine_ids.empty())
 		return crow::response(401);
 
 	// Extract optional filters
 	const auto type_id = CrowExtractionHelpers::extract_int_from_url_params(request, "type_id");
+	std::vector<alarm_activation_t> activations;
+	for (const auto machine_id : machine_ids | views::unique)
+		for (const auto& activation : alarm_storage->get_activations_for_machine(machine_id))
+			activations.push_back(activation);
 
-	const auto all_activations = alarm_storage->get_activations_for_machine(*machine_id);
-	const auto severities_of_active_alarms = all_activations
+	const auto severities_of_active_alarms = activations
 									   | views::filter([&](const auto& activation) { return !channel_id || activation.channel_id == *channel_id; })
 									   | views::filter([&](const auto& activation) { return !type_id || activation.type_id == *type_id; })
 									   | views::transform(&alarm_activation_t::severity)
